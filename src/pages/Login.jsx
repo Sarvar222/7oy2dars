eb5c856f213ea862c55d4b909b6bee8d0b820b48
sarvar <sarvar@gmail.com>
2025-01-14 20:52:49 +0500

    1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,32 +6,32 @@ function Login() {
   const { authenticateWithGoogle, isPending } = useAuthWithGoogle();
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
-      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg transform transition duration-500 hover:scale-105">
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500">
+      <div className="w-full max-w-lg bg-white p-8 rounded-lg shadow-2xl transform transition duration-500 hover:scale-105">
         <div className="text-center">
-          <h2 className="text-3xl font-bold text-gray-800 md:text-4xl">
+          <h2 className="text-4xl font-bold text-gray-800">
             Welcome Back!
           </h2>
-          <p className="mt-2 text-gray-600">Please login to continue</p>
+          <p className="mt-2 text-gray-600">Please login to access your account</p>
         </div>
-        <Form method="post" className="mt-6">
+        <Form method="post" className="mt-6 space-y-6">
           <FormInput
             label="Email"
             type="email"
             name="email"
-            size="input-sm md:input-md"
+            size="input-md"
             className="w-full"
           />
           <FormInput
             label="Password"
             type="password"
             name="password"
-            size="input-sm md:input-md"
-            className="w-full mt-4"
+            size="input-md"
+            className="w-full"
           />
-          <div className="mt-6 flex flex-col gap-3">
+          <div className="space-y-4">
             <button
-              className="btn w-full py-2 px-4 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 transition duration-300"
+              className="w-full py-3 px-4 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 transition duration-300"
               type="submit"
             >
               Login
@@ -40,7 +40,7 @@ function Login() {
               type="button"
               onClick={authenticateWithGoogle}
               disabled={isPending}
-              className={`btn w-full py-2 px-4 rounded-lg shadow-md ${
+              className={`w-full py-3 px-4 rounded-lg shadow-md ${
                 isPending
                   ? "bg-gray-300 text-gray-600 cursor-not-allowed"
                   : "bg-red-500 text-white hover:bg-red-600 hover:shadow-lg"
